d9b96f7282f874ab5ca0df58f32b008c215a0d13
Aditi Singh <aditisingh9@deloitte.com>
2022-08-03 21:30:03 +0530

    menu app

---
+++ src/data.js
@@ -0,0 +1,84 @@
+const menu = [
+    {
+        id : 1,
+        title : 'Chocolate Shake',
+        category : 'milkshake',
+        price : 60,
+        img : './images/chocolate_shake.jpeg',
+        description : 'The perfect sweet treat for any time of the day. Our chocolate shake is made with delicious soft serve, chocolate syrup and finished off with a creamy whipped topping.'
+    },
+    {
+        id : 2,
+        title : 'Bacon Sandwich',
+        category : 'breakfast',
+        price : 120,
+        img : './images/bacon_sandwich.JPG',
+        description : 'This Bacon, Muenster and Caramelized Onion Sandwich calls for a special cooking method. Run the tip of a plastic spatula or wooden spoon through egg yolks and the whites in a pan so that both parts of the egg become intertwined.'
+    },
+    {
+        id : 3,
+        title : 'Burger',
+        category : 'lunch',
+        price : 270,
+        img : './images/burger.JPG',
+        description : 'Ultimate Burger Press Burger that comes with Lettuce, Tomato, Onion, Special Sauce - Plus Mayo, Mustard or Ketchup.'
+    },
+    {
+        id : 4,
+        title : 'Dosa',
+        category : 'lunch',
+        price : 100,
+        img : './images/dosa.JPG',
+        description : 'South Indian style fermented crepe made with white lentil and rice-100% Gluten Free. (Dosas are served with home made Chutneys and Sambhar)'
+    },
+    {
+        id : 5,
+        title : 'Pancake',
+        category : 'breakfast',
+        price : 40,
+        img : './images/pancake.JPG',
+        description : 'One egg, your style four silver dollar pancakes choice of sausage or bacon.'
+    },
+    {
+        id : 6,
+        title : 'Salad',
+        category : 'lunch',
+        price : 120,
+        img : './images/salad.JPG',
+        description : 'A chunky salad of cucumbers, cherry tomatoes, peppery mint leaves drizzled with a black olive sauce.'
+    },
+    {
+        id : 7,
+        title : 'spaghetti',
+        category : 'dinner',
+        price : 350,
+        img : './images/spaghetti.JPG',
+        description : 'Mushrooms just need a quick sear in a hot pan before being tossed in this creamy (easy!) pasta sauce.'
+    },
+    {
+        id : 8,
+        title : 'Strawberry Shake',
+        category : 'milkshake',
+        price : 80,
+        img : './images/strawberry-shake.JPG',
+        description : 'The perfect sweet treat for any time of the day. Our strawberry shake is made with delicious soft serve, chocolate syrup and finished off with a creamy whipped topping.'
+    },
+    {
+        id : 9,
+        title : 'Taco',
+        category : 'dinner',
+        price : 90,
+        img : './images/taco.JPG',
+        description : 'A mix of Steak Fajita, Shrimp, Grilled Chicken & Sausage platter served with grilled onions, bell peppers, lettuce, pico de gallo, avocado, and a side of rice and beans.'
+    },
+    {
+        id : 10,
+        title : 'Vanilla Shake',
+        category : 'milkshake',
+        price : 70,
+        img : './images/vanilla_shake.JPG',
+        description : 'The perfect sweet treat for any time of the day. Our vanilla shake is made with delicious soft serve, chocolate syrup and finished off with a creamy whipped topping.'
+    },
+]
+
+export default menu
